b3b3d23054303726c77e3ac1165011ab0415e71d
Papposourusus <masikpatrik@gmail.com>
2025-04-29 15:10:16 +0200

    database

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
 <?php
+// index.php
 
-class Marathon {
-    public $id;
-    public $name;
-    public $date; 
+// Načítanie repozitára, ktorý zabezpečuje prácu s maratóny
+require_once 'classes/MarathonRepository.php';
 
-    public function __construct($id, $name, $date) {
-        $this->id = $id;
-        $this->name = $name;
-        $this->date = $date;
-    }
-}
-
-
-$marathons = [
-    new Marathon(1, 'Filmový maratón Akčné Filmy', '2025-05-10'),
-    new Marathon(2, 'Kultové Klasiky na noc', '2025-06-15'),
-    new Marathon(3, 'Sci-Fi Noc', '2025-07-20'),
-];
+$marathonRepo = new MarathonRepository();
+$marathons = $marathonRepo->getAll();
 ?>
 
 <!DOCTYPE html>
@@ -26,9 +14,9 @@ $marathons = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Filmové Maratóny</title>
-    
+    <!-- Link na externý CSS súbor -->
     <link rel="stylesheet" href="styles.css">
-    
+    <!-- Google Fonts pre moderný vzhľad -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 </head>
 <body>
