24728af36b4008c1686478d06c1624623d8a3d25
Papposourusus <masikpatrik@gmail.com>
2025-04-29 15:19:07 +0200

    filmy

---
+++ classes/Film.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Film {
+    public $id;
+    public $title;
+    public $description;
+    public $duration;
+    public $image;  
+
+    public function __construct($id, $title, $description, $duration, $image) {
+        $this->id = $id;
+        $this->title = $title;
+        $this->description = $description;
+        $this->duration = $duration;
+        $this->image = $image;
+    }
+}
+?>
